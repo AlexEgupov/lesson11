a7dabb1824149c95ba1fd6a7aa1dfd860ce7c077
AlexEgupov <alex.egupov7@gmail.com>
2022-06-09 12:51:43 +0300

    feat script.js lesson11

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+const eBtn = document.getElementById('e_btn');
+const range = document.getElementById('range');
+const circle = document.getElementById('circle');
+
+btn.addEventListener('click', () => {
+    btn.style.backgroundColor = text.value;
+});
+
+eBtn.style.display = 'none';
+
+range.addEventListener('input', () => {
+    circle.style.height = range.value + '%';
+    circle.style.width = range.value + '%';
+});
